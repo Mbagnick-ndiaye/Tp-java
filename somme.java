b1fafe409db704f993596a4752978657b9b41779
Mbagnick <mbagnickn828@gmail.com>
2024-10-17 15:13:53 +0000

    init

---
+++ somme.java
@@ -0,0 +1,11 @@
+class somme{
+    public static void main(String[]args){
+        int nombre1=10;
+        int nombre2=5;
+        int somme = nombre1 + nombre2;
+
+        System.out.println("La somme de "+ nombre1 +" et "+ nombre2 +" est:"+ somme);
+    }
+
+} 
+ 
